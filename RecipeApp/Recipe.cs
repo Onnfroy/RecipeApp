4baa5c11a822acf93f1524cc68e08ab374a88b05
Ethan Smith <ST10263534@vcconnect.edu.za>
2024-04-16 18:47:47 +0200

    Added the Recipe class with methods for adding ingredients, steps, and displaying the recipe.

--- RecipeApp/Recipe.cs
+++ RecipeApp/Recipe.cs
@@ -4,9 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+
 namespace RecipeApp
 {
-    internal class Class1
+    class Recipe
     {
+        private Ingredient[] ingredients;
+        private Step[] steps;
+        private int ingredientCount;
+        private int stepCount;
+
+        public Recipe(int maxIngredients, int maxSteps)
+        {
+            ingredients = new Ingredient[maxIngredients];
+            steps = new Step[maxSteps];
+            ingredientCount = 0;
+            stepCount = 0;
+        }
+
+        // Commit 2: Implemented Recipe Class
+        // Added the Recipe class with methods for adding ingredients, steps, and displaying the recipe.
+        public void AddIngredient(Ingredient ingredient)
+        {
+            if (ingredientCount < ingredients.Length)
+            {
+                ingredients[ingredientCount] = ingredient;
+                ingredientCount++;
+            }
+            else
+            {
+                Console.WriteLine("Cannot add more ingredients. Array is full.");
+            }
+        }
+
+        public void AddStep(Step step)
+        {
+            if (stepCount < steps.Length)
+            {
+                steps[stepCount] = step;
+                stepCount++;
+            }
+            else
+            {
+                Console.WriteLine("Cannot add more steps. Array is full.");
+            }
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine("Recipe:");
+            Console.WriteLine("Ingredients:");
+            for (int i = 0; i < ingredientCount; i++)
+            {
+                Console.WriteLine($"{i + 1}. {ingredients[i].Quantity} {ingredients[i].Unit} of {ingredients[i].Name}");
+            }
+            Console.WriteLine("Steps:");
+            for (int i = 0; i < stepCount; i++)
+            {
+                Console.WriteLine($"{i + 1}. {steps[i].Description}");
+            }
+        }
+
+        public void ScaleRecipe(double factor)
+        {
+            for (int i = 0; i < ingredientCount; i++)
+            {
+                ingredients[i].Quantity *= factor;
+            }
+        }
     }
 }
+

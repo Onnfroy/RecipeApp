f701282ce9ce9131bbd02147ef95bb6d6ad89b07
Ethan Smith <ST10263534@vcconnect.edu.za>
2024-05-30 16:09:02 +0200

    Added in Calories and Foogroup getters and setters as well as additional comments to ingredient class

--- RecipeApp/Ingredient.cs
+++ RecipeApp/Ingredient.cs
@@ -6,11 +6,35 @@ using System.Threading.Tasks;
 
 namespace RecipeApp
 {
-    class Ingredient
+    /// <summary>
+    /// Represents an ingredient in a recipe.
+    /// </summary>
+    public class Ingredient
     {
+        /// <summary>
+        /// Gets or sets the name of the ingredient.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity of the ingredient.
+        /// </summary>
         public double Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unit of measurement for the ingredient.
+        /// </summary>
         public string Unit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the calorie count of the ingredient.
+        /// </summary>
+        public int Calories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the food group of the ingredient.
+        /// </summary>
+        public string FoodGroup { get; set; }
     }
 }
 ///////////////////////////////////////////////////////////////////////End Of File///////////////////////////////////////////////////////////////////////////////////////

934dc02deb7a1cba3707ce2c32b337a3d7de1004
Ethan Smith <ST10263534@vcconnect.edu.za>
2024-06-26 16:29:14 +0200

    Added a LoadRecipe method which loads the recipes into the listbox as well as a generatePieChart event which generates a pie chart based off of the selected recipes, ingredients and food groups

--- RecipeAppWPF/CreateMenuPage.xaml.cs
+++ RecipeAppWPF/CreateMenuPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RecipeApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,51 @@ namespace RecipeAppWPF
         public CreateMenuPage()
         {
             InitializeComponent();
+            LoadRecipes();
+        }
+
+        private void LoadRecipes()
+        {
+            var recipes = Program.recipes.OrderBy(r => r.Name).ToList();
+            RecipesListBox.ItemsSource = recipes;
+        }
+
+        private void GeneratePieChart_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedRecipes = RecipesListBox.SelectedItems.Cast<Recipe>().ToList();
+            if (selectedRecipes.Count == 0)
+            {
+                MessageBox.Show("Please select at least one recipe.");
+                return;
+            }
+
+            var foodGroupCounts = new Dictionary<string, double>();
+
+            foreach (var recipe in selectedRecipes)
+            {
+                foreach (var ingredient in recipe.GetIngredients())
+                {
+                    if (foodGroupCounts.ContainsKey(ingredient.FoodGroup))
+                    {
+                        foodGroupCounts[ingredient.FoodGroup] += ingredient.Quantity;
+                    }
+                    else
+                    {
+                        foodGroupCounts[ingredient.FoodGroup] = ingredient.Quantity;
+                    }
+                }
+            }
+
+            FoodGroupChart.Series.Clear();
+            foreach (var foodGroup in foodGroupCounts)
+            {
+                var pieSeries = new PieSeries
+                {
+                    Title = foodGroup.Key,
+                    Values = new ChartValues<double> { foodGroup.Value }
+                };
+                FoodGroupChart.Series.Add(pieSeries);
+            }
         }
     }
 }

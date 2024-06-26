6bd280b6dcf9c0b924ae38367eba35ba88aaa188
Ethan Smith <ST10263534@vcconnect.edu.za>
2024-06-26 16:12:49 +0200

    Included event handlers in the AddRecipePage.cs class for the addition of ingredients

--- RecipeAppWPF/AddRecipePage.xaml.cs
+++ RecipeAppWPF/AddRecipePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RecipeApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,29 @@ namespace RecipeAppWPF
         public AddRecipePage()
         {
             InitializeComponent();
+            recipe = new Recipe();
+        }
+
+        private void AddIngredient_Click(object sender, RoutedEventArgs e)
+        {
+            Ingredient ingredient = new Ingredient
+            {
+                Name = IngredientName.Text,
+                Quantity = double.Parse(Quantity.Text),
+                Unit = Unit.Text,
+                Calories = int.Parse(Calories.Text),
+                FoodGroup = FoodGroup.Text
+            };
+            recipe.AddIngredient(ingredient);
+            MessageBox.Show("Ingredient added.");
+        }
+
+        private void SaveRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            recipe.Name = RecipeName.Text;
+            Program.recipes.Add(recipe); // Ensure Program.recipes is public or modify as needed.
+            MessageBox.Show("Recipe saved.");
         }
     }
-}
+    }
+

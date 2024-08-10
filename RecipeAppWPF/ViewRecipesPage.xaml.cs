197c72419e114b04bd021ab04ea82cf8804f5d54
Ethan Smith <ST10263534@vcconnect.edu.za>
2024-08-10 18:09:54 +0200

    Added in EOF comments

--- RecipeAppWPF/ViewRecipesPage.xaml.cs
+++ RecipeAppWPF/ViewRecipesPage.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using RecipeApp;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 
 namespace RecipeAppWPF
@@ -48,3 +36,5 @@ namespace RecipeAppWPF
         }
     }
 }
+
+//--------------------------------------------------------EOF--------------------------------------------------------------------------------//
